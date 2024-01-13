7ffab019cdd2150d645388705730e50bc94ee978
nedate1976 <ndate1976@hotmail.com>
2024-01-13 14:51:42 +0530

    initial commit

---
+++ src/main/java/com/saadhak/app/list/upload/SaadhakAppListUploadApplication.java
@@ -0,0 +1,13 @@
+package com.saadhak.app.list.upload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaadhakAppListUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaadhakAppListUploadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saadhak/app/list/upload/SaadhakAppListUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saadhak.app.list.upload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaadhakAppListUploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
